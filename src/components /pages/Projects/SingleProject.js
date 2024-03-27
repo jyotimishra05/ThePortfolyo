91bb41abe93a0ec00511829da5fbc5954b16b264
galaxy28 <51984459+jyotimishra05@users.noreply.github.com>
2024-03-27 20:49:52 +0530

    Update SingleProject.js

--- src/components /pages/Projects/SingleProject.js
+++ src/components /pages/Projects/SingleProject.js
@@ -1,35 +1,33 @@
-import React, { useState } from 'react'
-import MoreDetail from './MoreDetail';
+import React, { useState } from "react";
+import MoreDetail from "./MoreDetail";
 
-const SingleProject = ({project,index}) => {
-    const [showCard, setShowCard] = useState(false);
-    // console.log("data",data.reverse())
-    const handleClick = () => {
-      setShowCard(true);
-      console.log("clicked");
-    };
+const SingleProject = ({ project }) => {
+  const [showCard, setShowCard] = useState(false);
+
+  const handleClick = () => {
+    setShowCard(true);
+  };
+
   return (
-    <div >
-      <div
-        
-        key={index}
-        style={{ display: showCard ? "none" : "block" }}
-      >
+    <div>
+      <div style={{ display: showCard ? "none" : "block" }}>
         <img
           src={project.image.url}
-          className="card-img-top mb-4 mt-4"
+          className="card-img-top mb-4 mt-4 border border-warning p-2"
           alt="Card"
         />
-
+        <div className="text-white fs-4 fw-bold text-center mb-4 mt-4">
+          {project.title}
+        </div>
         <div className="button-container text-center">
-          <button className="btn btn-warning" onClick={handleClick}>
-            {project.title}
+          <button className="btn btn-warning mb-4" onClick={handleClick}>
+            Click For More Detail
           </button>
         </div>
       </div>
-      {showCard && <MoreDetail setShowCard={setShowCard} project={project}/>}
+      {showCard && <MoreDetail setShowCard={setShowCard} project={project} />}
     </div>
   );
-}
+};
 
-export default SingleProject
+export default SingleProject;

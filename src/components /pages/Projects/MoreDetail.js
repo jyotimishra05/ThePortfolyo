42bf6665291b7cdd376e4a34fb8eb49f981e6b94
galaxy28 <51984459+jyotimishra05@users.noreply.github.com>
2024-03-27 20:50:24 +0530

    Update MoreDetail.js

--- src/components /pages/Projects/MoreDetail.js
+++ src/components /pages/Projects/MoreDetail.js
@@ -2,15 +2,18 @@ import React from 'react'
 
 const MoreDetail = ({ setShowCard,project }) => {
   return (
-    <div className="detailCard text-white d-flex flex-column justify-content-center ">
+    <div className=" text-white d-flex flex-column justify-content-center border border-warning mt-4">
       <p className="fw-bold ms-3 mt-4 ">TechStack : {project.techStack}</p>
-      <p className='ms-3'>
+      <p className="ms-3">
         Description:
         {project.description
           ? project.description
           : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel massa quis eros feugiat aliquet. Nullam a libero facilisis mi placerat suscipit eu vitae ex."}
       </p>
-      <button className="btn btn-primary m-5" onClick={() => setShowCard(false)}>
+      <button
+        className="btn btn-primary m-5"
+        onClick={() => setShowCard(false)}
+      >
         Go-Back
       </button>
     </div>
